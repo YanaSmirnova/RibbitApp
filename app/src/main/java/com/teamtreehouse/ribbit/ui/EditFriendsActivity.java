a02677e5a1423fce12f23590a8cf4d33f8c3d8f4
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-23 15:57:00 +1100

    Created a reusable GridView layout component to use within EditFriendsActivity and RecipientsActivity

--- app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.ribbit.ui;
 
 import java.util.List;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
@@ -11,7 +12,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -19,13 +22,15 @@ import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.teamtreehouse.ribbit.adapters.UserAdapter;
 import com.teamtreehouse.ribbit.utils.ParseConstants;
 import com.teamtreehouse.ribbit.R;
 
-public class EditFriendsActivity extends ListActivity {
+public class EditFriendsActivity extends Activity {
 	
 	protected ParseRelation<ParseUser> mFriendsRelation;
 	protected ParseUser mCurrentUser;
+    protected GridView mGridView;
 	
 	public static final String TAG = EditFriendsActivity.class.getSimpleName();
 	
@@ -35,11 +40,15 @@ public class EditFriendsActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		setContentView(R.layout.activity_edit_friends);
+		setContentView(R.layout.user_grid);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
-		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        mGridView = (GridView)findViewById(R.id.friendsGrid);
+		mGridView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE);
+
+        TextView emptyTextView = (TextView)findViewById(android.R.id.empty);
+        mGridView.setEmptyView(emptyTextView);
 	}
 	
 	@Override
@@ -68,11 +77,14 @@ public class EditFriendsActivity extends ListActivity {
 						usernames[i] = user.getUsername();
 						i++;
 					}
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-							EditFriendsActivity.this, 
-							android.R.layout.simple_list_item_checked,
-							usernames);
-					setListAdapter(adapter);
+
+                    if (mGridView.getAdapter() == null) {
+                        UserAdapter adapter = new UserAdapter(EditFriendsActivity.this, mUsers);
+                        mGridView.setAdapter(adapter);
+                    }
+                    else {
+                        ((UserAdapter)mGridView.getAdapter()).refill(mUsers);
+                    }
 					
 					addFriendCheckmarks();
 				}
@@ -115,7 +127,7 @@ public class EditFriendsActivity extends ListActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
+/*	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		
@@ -136,7 +148,7 @@ public class EditFriendsActivity extends ListActivity {
 				}
 			}
 		});
-	}
+	}*/
 	
 	private void addFriendCheckmarks() {
 		mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
@@ -149,7 +161,7 @@ public class EditFriendsActivity extends ListActivity {
 						
 						for (ParseUser friend : friends) {
 							if (friend.getObjectId().equals(user.getObjectId())) {
-								getListView().setItemChecked(i, true);
+								mGridView.setItemChecked(i, true);
 							}
 						}
 					}
