7aceeece65a2a2c60634ee8ac944acfa4f003e6e
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-23 20:27:41 +1100

    Added a listener to tap on items in the grid view

--- app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
+++ app/src/main/java/com/teamtreehouse/ribbit/ui/EditFriendsActivity.java
@@ -11,8 +11,10 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -46,6 +48,7 @@ public class EditFriendsActivity extends Activity {
 
         mGridView = (GridView)findViewById(R.id.friendsGrid);
 		mGridView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE);
+        mGridView.setOnItemClickListener(mOnItemClickListener);
 
         TextView emptyTextView = (TextView)findViewById(android.R.id.empty);
         mGridView.setEmptyView(emptyTextView);
@@ -127,29 +130,6 @@ public class EditFriendsActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-/*	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		
-		if (getListView().isItemChecked(position)) {
-			// add the friend
-			mFriendsRelation.add(mUsers.get(position));
-		}
-		else {
-			// remove the friend
-			mFriendsRelation.remove(mUsers.get(position));
-		}
-
-		mCurrentUser.saveInBackground(new SaveCallback() {
-			@Override
-			public void done(ParseException e) {
-				if (e != null) {
-					Log.e(TAG, e.getMessage());
-				}
-			}
-		});
-	}*/
-	
 	private void addFriendCheckmarks() {
 		mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
 			@Override
@@ -172,6 +152,34 @@ public class EditFriendsActivity extends Activity {
 			}
 		});
 	}
+
+    protected AdapterView.OnItemClickListener mOnItemClickListener =
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    ImageView checkImageView = (ImageView)view.findViewById(R.id.checkImageView);
+
+                    if (mGridView.isItemChecked(position)) {
+                        // add the friend
+                        mFriendsRelation.add(mUsers.get(position));
+                        checkImageView.setVisibility(View.VISIBLE);
+                    }
+                    else {
+                        // remove the friend
+                        mFriendsRelation.remove(mUsers.get(position));
+                        checkImageView.setVisibility(View.INVISIBLE);
+                    }
+
+                    mCurrentUser.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Log.e(TAG, e.getMessage());
+                            }
+                        }
+                    });
+                }
+            };
 }
 
 
