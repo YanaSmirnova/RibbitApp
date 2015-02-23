aefb207865745598ccffecce965e407fb4f48f44
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-23 16:57:06 +1100

    Added a checkmark overlay in the GridView for EditFriendsActivity

--- app/src/main/java/com/teamtreehouse/ribbit/adapters/UserAdapter.java
+++ app/src/main/java/com/teamtreehouse/ribbit/adapters/UserAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,6 +36,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
 			convertView = LayoutInflater.from(mContext).inflate(R.layout.user_item, null);
 			holder = new ViewHolder();
 			holder.userImageView = (ImageView)convertView.findViewById(R.id.userImageView);
+            holder.checkImageView = (ImageView)convertView.findViewById(R.id.checkImageView);
 			holder.nameLabel = (TextView)convertView.findViewById(R.id.nameLabel);
 			convertView.setTag(holder);
 		}
@@ -58,12 +60,21 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
         }
 
 		holder.nameLabel.setText(user.getUsername());
+
+        GridView gridView = (GridView) parent;
+        if (gridView.isItemChecked(position)) {
+            holder.checkImageView.setVisibility(View.VISIBLE);
+        }
+        else {
+            holder.checkImageView.setVisibility(View.INVISIBLE);
+        }
 		
 		return convertView;
 	}
 	
 	private static class ViewHolder {
 		ImageView userImageView;
+        ImageView checkImageView;
 		TextView nameLabel;
 	}
 	
