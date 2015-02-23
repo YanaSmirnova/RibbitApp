d69201d53aa6e2de6aa34a4acb6b7226cc0a1d81
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-23 22:48:04 +1100

    Set Up Push Notifications with Parse

--- app/src/main/java/com/teamtreehouse/ribbit/RibbitApplication.java
+++ app/src/main/java/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -10,8 +10,8 @@ public class RibbitApplication extends Application {
 	@Override
 	public void onCreate() { 
 		super.onCreate();
-	    Parse.initialize(this, 
-	    	"EiK2zlnCPYFlDQjRPDd8Zr6vyg3YYih8W90emAg3",
-	    	"jZg8ziWROu6CydqOzFBbWjpkwyMb0nWEx2MVVGDg");
+        Parse.initialize(this,
+                "EiK2zlnCPYFlDQjRPDd8Zr6vyg3YYih8W90emAg3",
+                "jZg8ziWROu6CydqOzFBbWjpkwyMb0nWEx2MVVGDg");
 	}
 }
